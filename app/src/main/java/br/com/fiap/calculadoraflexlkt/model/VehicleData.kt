0bcb7db629a29dd3a7cd5aef572bbb39f18ba09a
Logon Aluno <logonaluno@local.com>
2019-08-17 16:40:01 -0300

    Envio dos dados do veiculos para o firebase

---
+++ app/src/main/java/br/com/fiap/calculadoraflexlkt/model/VehicleData.kt
@@ -0,0 +1,8 @@
+package br.com.fiap.calculadoraflexlkt.model
+
+data class VehicleData(
+    val gasPrice: Double = 0.0,
+    val ethanolPrice: Double = 0.0,
+    val gasAverage: Double = 0.0,
+    val ethanolAverage: Double = 0.0
+)
--- app/src/main/java/br/com/fiap/calculadoraflexlkt/ui/form/FormActivity.kt
+++ app/src/main/java/br/com/fiap/calculadoraflexlkt/ui/form/FormActivity.kt
@@ -3,16 +3,34 @@ package br.com.fiap.calculadoraflexlkt.ui.form
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import br.com.fiap.calculadoraflexlkt.R
+import br.com.fiap.calculadoraflexlkt.model.VehicleData
+import br.com.fiap.calculadoraflexlkt.ui.login.LoginActivity
 import br.com.fiap.calculadoraflexlkt.ui.result.ResultActivity
+import br.com.fiap.calculadoraflexlkt.utils.DatabaseUtil
 import br.com.fiap.calculadoraflexlkt.watchers.DecimalTextWatcher
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_form.*
 
 class FormActivity : AppCompatActivity() {
 
+    private lateinit var userId: String
+    private lateinit var mAuth: FirebaseAuth
+    private val firebaseReferenceNode = "VehicleData"
+    private val defaultClearValueText = "0.0"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+        mAuth = FirebaseAuth.getInstance()
+        userId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+        listenerFirebaseRealtime()
 
         etGasPrice.addTextChangedListener(DecimalTextWatcher(etGasPrice))
         etEthanolPrice.addTextChangedListener(DecimalTextWatcher(etEthanolPrice))
@@ -27,6 +45,73 @@ class FormActivity : AppCompatActivity() {
             nextScreen.putExtra("ETHANOL_AVERAGE", etEthanolAverage.text.toString().toDouble())
             startActivity(nextScreen)
         }
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.form_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            R.id.action_clear -> {
+                clearData()
+                return true
+            }
+            R.id.action_logout -> {
+                logout()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    private fun saveCarData() {
+        val carData = VehicleData(
+            etGasPrice.text.toString().toDouble(),
+            etEthanolPrice.text.toString().toDouble(),
+            etGasAverage.text.toString().toDouble(),
+            etEthanolAverage.text.toString().toDouble()
+        )
+        FirebaseDatabase.getInstance().getReference(firebaseReferenceNode)
+            .child(userId)
+            .setValue(carData)
+    }
+
+    private fun listenerFirebaseRealtime() {
+        DatabaseUtil.getDatabase()
+            .getReference(firebaseReferenceNode)
+            .child(userId)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val carData =
+                        dataSnapshot.getValue(VehicleData::class.java)
+                    etGasPrice.setText(carData?.gasPrice.toString())
+
+                    etEthanolPrice.setText(carData?.ethanolPrice.toString())
+                    etGasAverage.setText(carData?.gasAverage.toString())
+
+                    etEthanolAverage.setText(carData?.ethanolAverage.toString())
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+    }
+
+    private fun logout() {
+        mAuth.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
+    private fun clearData() {
+        etGasPrice.setText(defaultClearValueText)
+        etEthanolPrice.setText(defaultClearValueText)
+        etGasAverage.setText(defaultClearValueText)
+        etEthanolAverage.setText(defaultClearValueText)
+    }
+
 }
