3935ee68ed4dd73151388465ad9876314b798a20
Logon Aluno <logonaluno@local.com>
2019-08-17 16:09:32 -0300

    Funções de calculos

--- app/src/main/java/br/com/fiap/calculadoraflexlkt/ui/form/FormActivity.kt
+++ app/src/main/java/br/com/fiap/calculadoraflexlkt/ui/form/FormActivity.kt
@@ -1,11 +1,32 @@
 package br.com.fiap.calculadoraflexlkt.ui.form
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fiap.calculadoraflexlkt.R
+import br.com.fiap.calculadoraflexlkt.ui.result.ResultActivity
+import br.com.fiap.calculadoraflexlkt.watchers.DecimalTextWatcher
+import kotlinx.android.synthetic.main.activity_form.*
 
 class FormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+
+        etGasPrice.addTextChangedListener(DecimalTextWatcher(etGasPrice))
+        etEthanolPrice.addTextChangedListener(DecimalTextWatcher(etEthanolPrice))
+        etGasAverage.addTextChangedListener(DecimalTextWatcher(etGasAverage))
+        etEthanolAverage.addTextChangedListener(DecimalTextWatcher(etEthanolAverage))
+
+        btCalculate.setOnClickListener{
+            val nextScreen = Intent(this@FormActivity, ResultActivity::class.java)
+            nextScreen.putExtra("GAS_PRICE", etGasPrice.text.toString().toDouble())
+            nextScreen.putExtra("ETHANOL_PRICE", etEthanolPrice.text.toString().toDouble())
+            nextScreen.putExtra("GAS_AVERAGE", etGasAverage.text.toString().toDouble())
+            nextScreen.putExtra("ETHANOL_AVERAGE", etEthanolAverage.text.toString().toDouble())
+            startActivity(nextScreen)
+        }
+
     }
 }
